2bd842b4378e842a274a4f3e309e29df70ae0131
Amman Hossain <ammanhossain908@gmail.com>
2021-06-10 00:45:22 +0600

    add changes

--- my_app/views.py
+++ my_app/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from bs4 import BeautifulSoup
+import requests
+from requests.compat import quote_plus
+from . import models
 
 # Create your views here.
+
+BASE_DARAZ_URL='https://www.daraz.com.bd/catalog/?q={}'
 
 
 def home(request):
@@ -9,8 +14,19 @@ def home(request):
     return render(request, 'base.html')
 
 def new_search(request):
-
+    
     search = request.POST.get('search')
+    models.Search.objects.create( search=search )
+
+    #print(quote_plus(search))
+    final_url = BASE_DARAZ_URL.format(quote_plus(search))
+    print(final_url)
+    response = requests.get(final_url)
+    data =response.text
+    soup = BeautifulSoup(data, features = 'html.parser')
+    post_titles= soup.find('a').get('title')
+    print(post_titles)
+    #print(data)
     stuff_for_fontend={
         'search':search,
     }
